d856b156eace13f043168088e9efd4b98a8c3d2d
roza <mirian_mrh@hotmail.com>
2011-09-16 23:40:06 -0500

    Primer ejercicio con control de versiones git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '411bbdbc7736d3b35db805e087e7e42a6efb85300182143a38b425f63b7cc64676920419d1a32ccd97fdd620a4f05ca8bedfcb676ddbc51003303b365754c2de'
